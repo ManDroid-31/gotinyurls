b34541e0171780b4eb34a77b4fe0254385f75302
omkarghodekar140803@gmail.com <omkarghodekar140803@gmail.com>
2025-08-18 01:30:28 +0530

    url table fix

--- Backend/index.js
+++ Backend/index.js
@@ -16,18 +16,8 @@ app.use(cookieParser());
 
 const allowedOrigins = [process.env.FRONTEND_URL];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true
-  })
-);
+app.use(cors({ origin: "*" }));
+
 
 
 app.get("/", (_req, res) => res.send("Shortify API OK"));
