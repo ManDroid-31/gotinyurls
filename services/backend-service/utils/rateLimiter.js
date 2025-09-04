2809e81948098be2995115a9c43fc67186771caa
omkarghodekar140803@gmail.com <omkarghodekar140803@gmail.com>
2025-09-04 22:59:27 +0530

    rate limiter

--- services/backend-service/utils/rateLimiter.js
+++ services/backend-service/utils/rateLimiter.js
@@ -1,6 +1,10 @@
 const RATE_LIMIT_WINDOW = 60;
 const MAX_REQUESTS = 20;
 
+import { redisConnect } from "shared";
+
+const redis = redisConnect();
+
 export const rateLimit = async (ip) => {
   const key = `rl:${ip}`;
   const count = await redis.incr(key);
